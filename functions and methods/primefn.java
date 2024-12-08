ba6a665d5b7ce2f9133d7e7c455e3998dc669b86
neeraj kushwaha <workwithneeraj.01@gmail.com>
2024-12-08 23:25:30 +0530

    primefn added question

--- functions and methods/primefn.java
+++ functions and methods/primefn.java
@@ -1,49 +1,61 @@
-// import java.util.*;
-// public class primefn {
-//     public static boolean isPrime(int n){
-//         boolean isPrime =true;
-//         for(int i =2;i<=n-1;i++){
-//             if(n%2==0){    // completely dividing 
-//                 isPrime =false;
-//             }
-//         }
-//         return isPrime;
-//     }
-//     public static void main(String[] args) {
-//         Scanner sc =new Scanner(System.in);
-//         System.out.println("enter your number");
-//         int n =sc.nextInt();
-//         System.out.println(isPrime(n));
+import java.util.*;
+public class primefn {
+    public static boolean isPrime(int n){
+        boolean isPrime =true;
+        for(int i=2; i<=n-1; i++){
+            if(n%2==0){    // completely dividing 
+                isPrime =false;
+            }
+        }
+        return isPrime;
+    }
 
-//     }
+    public static void primeRange(int n){
+        for(int i=2;i<=n;i++){
+            if(isPrime(i)){
+                System.out.println(i+ " ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        // Scanner sc =new Scanner(System.in);
+        // System.out.println("enter your number");
+        // int n =sc.nextInt();
+        // System.out.println(isPrime(n));
+
+        primeRange(20);
+
+    }
+}
   
 // ----------------------*******************************-----------------------------
 
-import java.util.*;
-import java.util.Scanner;
-public class primefn {
-    public static boolean isPrime(int num){
+// import java.util.*;
+// import java.util.Scanner;
+// public class primefn {
+//     public static boolean isPrime(int num){
         
-        for(int i=2;i<=num-1;i++){
-            if(num%2==0){
-                return false;
-            }
-        }
-        return true;
-    }
+//         for(int i=2;i<=num-1;i++){
+//             if(num%2==0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter your number");
-        int n =sc.nextInt();
-        int count = 0;
-        int num =2;
-        while (count<=n) {
-            if(isPrime(num)){
-                System.out.println("num"+num);
-                count++;
-            }
-            num++;
-        }
-    }
-}
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter your number");
+//         int n =sc.nextInt();
+//         int count = 0;
+//         int num =2;
+//         while (count<=n) {
+//             if(isPrime(num)){
+//                 System.out.println("num"+num);
+//                 count++;
+//             }
+//             num++;
+//         }
+//     }
+// }
