a87f77085dc00e23ecdf0ef0141f1ed704baf5cd
neeraj kushwaha <workwithneeraj.01@gmail.com>
2024-12-27 08:41:17 +0530

    inverted half pyramid triangle

---
+++ advanced pattern/halfPyramid.java
@@ -0,0 +1,21 @@
+// package advanced pattern;
+
+public class halfPyramid {
+    public static void printHalfPyramid(int n){
+        //lines
+        for(int i=1; i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        printHalfPyramid(4);
+    }
+}
